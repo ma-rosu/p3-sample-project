ef8212ff9ee7454b279549d890b91dce9a9342f0
Marius <ma.rosu06@gmail.com>
2023-10-23 12:13:48 +0300

    Commit fox = red lazy = active

--- src/ro/uvt/p3/io/InputDevice.java
+++ src/ro/uvt/p3/io/InputDevice.java
@@ -3,6 +3,6 @@ package ro.uvt.p3.io;
 public class InputDevice {
 
     public String nextLine(){
-        return "The quick red fox jumps over the active dog.";
+        return "Raul jumps over Teo.";
     }
 }
